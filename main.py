af66fb8f4f3ece202719386f9eb267b404732d48
Finely Clapper <71201242+FinleyClapper@users.noreply.github.com>
2026-02-20 17:38:56 -0500

    Add state table

--- main.py
+++ main.py
@@ -88,4 +88,43 @@ def gen_table2(db: pd.DataFrame) -> pd.DataFrame:
     table2 = pd.concat(all_vars)               
     return table2
 
+def gen_state_table(db: pd.DataFrame) -> pd.DataFrame:
+
+    #dict to translate codes to state names
+    names = {
+    1: 'Alabama', 2: 'Alaska', 4: 'Arizona', 5: 'Arkansas', 6: 'California',
+    8: 'Colorado', 9: 'Connecticut', 10: 'Delaware', 11: 'District of Columbia',
+    12: 'Florida', 13: 'Georgia', 15: 'Hawaii', 16: 'Idaho', 17: 'Illinois',
+    18: 'Indiana', 19: 'Iowa', 20: 'Kansas', 21: 'Kentucky', 22: 'Louisiana',
+    23: 'Maine', 24: 'Maryland', 25: 'Massachusetts', 26: 'Michigan',
+    27: 'Minnesota', 28: 'Mississippi', 29: 'Missouri', 30: 'Montana',
+    31: 'Nebraska', 32: 'Nevada', 33: 'New Hampshire', 34: 'New Jersey',
+    35: 'New Mexico', 36: 'New York', 37: 'North Carolina', 38: 'North Dakota',
+    39: 'Ohio', 40: 'Oklahoma', 41: 'Oregon', 42: 'Pennsylvania', 44: 'Rhode Island',
+    45: 'South Carolina', 46: 'South Dakota', 47: 'Tennessee', 48: 'Texas',
+    49: 'Utah', 50: 'Vermont', 51: 'Virginia', 53: 'Washington', 54: 'West Virginia',
+    55: 'Wisconsin', 56: 'Wyoming'
+}
+    #get counts and percents for states
+    state_counts = db['statefips'].value_counts()
+    state_percents = (db['statefips'].value_counts(normalize=True) * 100).round(2)
+    
+    #make table with all state counts and percents
+    states = pd.DataFrame({
+        'Frequency': state_counts,
+        'Percent': state_percents
+    }).sort_values('Frequency', ascending=False)
+
+    #get top and bottom 5
+    top5 = states.head(5)
+    bottom5 = states.tail(5)
+    
+    #create final state table
+    state_table = pd.concat([top5, bottom5], keys=['Top 5 States', 'Bottom 5 States'])
+    state_table = state_table.reset_index()
+    state_table['statefips'] = state_table['statefips'].map(names)
+    state_table.columns = ['Group', 'State', 'Frequency', 'Percent']
+    return state_table
+
 print(gen_table2(db))
+print(gen_state_table(db))
